21cb49f1c947c84d58812e323ccc4ee2cdd4e168
Rayched <ny2401@naver.com>
2024-03-02 00:03:02 +0900

    publishing Setting

--- src/routes/MovieDetail.js
+++ src/routes/MovieDetail.js
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function MovieDetail(){
+    const [MovieInfo, setMovieInfo] = useState([]);
+    const [DirName, setDirName] = useState();
     const {id} = useParams();
-    console.log(id);
+
+    const getMovieDetail = async() => {
+        const MovieData= await(
+            await fetch(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=3a15c5393ac14d11f6b132d6a07f330c&movieCd=${id}`)
+        ).json();
+        setMovieInfo(MovieData);
+    };
+
+    useEffect(() => {
+        getMovieDetail();
+    }, []);
+
+    setDirName(MovieInfo["directors"]["peopleNm"]);
+
+    console.log(MovieInfo);
 
     return (
         <div>
-            <h4>Detail</h4>
+            <h3>영화 명: {MovieInfo.movieNm}</h3>
+            <p>개봉 일: {MovieInfo.prdtYear}년</p>
+            <p>상영 시간: {MovieInfo.showTm}분</p>
+            <p>감독: </p>
         </div>
     );
 }
